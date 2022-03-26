3af01a95a06f3572404a4ed1c26a1a01e0e50a45
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-03-26 22:40:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++ {
+      if (arr[i] + arr[j] == value)
+        count += 1;
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int t;
+  for (int i = len - 1; i >= 0; i--) {
+    if (arr[i] > value) {
+      continue;
+    } else {
+      t = i;
+      break;
+    }
+  }
+  for (int i = 0; i < t - 1; i++) {
+    for (int j = t; j >= i + 1; j--) {
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0, Lbound = 0, RBound = len - 1;
+  while (LBound, Rbound - 1) {
+    int middle_1 = (LBound + RBound) / 2;
+    if (arr[middle_1] <= value)
+      Lbound = middle_1;
+    else
+      RBound = middle_1;
+  }
+  len = Rbound - 1;
+  for (int i = 0; i < len; i++) {
+    Lbound = i + 1, RBound = len - 1;
+    int k = 0;
+    while (LBound < Rbound) {
+      int middle_2 = (LBound + RBound) / 2;
+      if (arr[middle_2] < (value - arr[i]))
+        LBound = middle_2 + 1;
+      else
+        RBound = middle_2;
+    }
+    while (arr[LBound] == (value - arr[i])) {
+      k++;
+      LBound++;
+    }
+    count += k;
+  }
+  return count;
 }
